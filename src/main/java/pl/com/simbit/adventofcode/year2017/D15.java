e855952ffcd74146df0256e7154917868864a672
Marcin Krajewski <mkrajewski.work@gmail.com>
2017-12-15 22:28:57 +0100

    refactor day 15 code (year 2017)

--- src/main/java/pl/com/simbit/adventofcode/year2017/D15.java
+++ src/main/java/pl/com/simbit/adventofcode/year2017/D15.java
@@ -3,41 +3,74 @@ package pl.com.simbit.adventofcode.year2017;
 import java.math.BigInteger;
 
 import pl.com.simbit.adventofcode.Day;
+import pl.com.simbit.utility.math.BinaryNumber;
 
 public class D15 implements Day {
 
 	public static final BigInteger MUL_A = BigInteger.valueOf(16807);
 	public static final BigInteger MUL_B = BigInteger.valueOf(48271);
 	public static final BigInteger MOD = BigInteger.valueOf(2147483647);
-	public static final int MOD2 = 65536;
-	private int startA = 618;
-	private int startB = 814;
+	public static final BigInteger MODA = BigInteger.valueOf(4);
+	public static final BigInteger MODB = BigInteger.valueOf(8);
+	private BigInteger startA = BigInteger.valueOf(618);
+	private BigInteger startB = BigInteger.valueOf(814);
 
 	@Override
 	public Object problem1() {
+		return resolveProblem(new P1(), 40000000);
+	}
+
+	@Override
+	public Object problem2() {
+		return resolveProblem(new P2(), 5000000);
+	}
+
+	public int resolveProblem(NextNumGetter valueGetter, int limit) {
 		int count = 0;
-		for (int i = 0; i < 40000000; i++) {
-			startA = MUL_A.multiply(BigInteger.valueOf(startA)).mod(MOD).intValue();
-			startB = MUL_B.multiply(BigInteger.valueOf(startB)).mod(MOD).intValue();
-			if (startA % MOD2 == startB % MOD2)
+		for (int i = 0; i < limit; i++) {
+			startA = valueGetter.nextValueA(startA);
+			startB = valueGetter.nextValueB(startB);
+			if (BinaryNumber.hasTheSameLastBits(startA.intValue(), startB.intValue(), 16))
 				count++;
 		}
 		return count;
 	}
 
-	@Override
-	public Object problem2() {
-		int count = 0;
-		for (int i = 0; i < 5000000; i++) {
-			startA = MUL_A.multiply(BigInteger.valueOf(startA)).mod(MOD).intValue();
-			while (!(startA % 4 == 0))
-				startA = MUL_A.multiply(BigInteger.valueOf(startA)).mod(MOD).intValue();
-			startB = MUL_B.multiply(BigInteger.valueOf(startB)).mod(MOD).intValue();
-			while (!(startB % 8 == 0))
-				startB = MUL_B.multiply(BigInteger.valueOf(startB)).mod(MOD).intValue();
-			if (startA % MOD2 == startB % MOD2)
-				count++;
+	interface NextNumGetter {
+		BigInteger nextValueA(BigInteger previousValue);
+
+		BigInteger nextValueB(BigInteger previousValue);
+	}
+
+	class P1 implements NextNumGetter {
+
+		@Override
+		public BigInteger nextValueA(BigInteger previousValue) {
+			return MUL_A.multiply(previousValue).mod(MOD);
 		}
-		return count;
+
+		@Override
+		public BigInteger nextValueB(BigInteger previousValue) {
+			return MUL_B.multiply(previousValue).mod(MOD);
+		}
+	}
+
+	class P2 implements NextNumGetter {
+
+		@Override
+		public BigInteger nextValueA(BigInteger previousValue) {
+			previousValue = MUL_A.multiply(previousValue).mod(MOD);
+			while (!(previousValue.mod(MODA) == BigInteger.ZERO))
+				previousValue = MUL_A.multiply(previousValue).mod(MOD);
+			return previousValue;
+		}
+
+		@Override
+		public BigInteger nextValueB(BigInteger previousValue) {
+			previousValue = MUL_B.multiply(previousValue).mod(MOD);
+			while (!(previousValue.mod(MODB) == BigInteger.ZERO))
+				previousValue = MUL_B.multiply(previousValue).mod(MOD);
+			return previousValue;
+		}
 	}
 }
